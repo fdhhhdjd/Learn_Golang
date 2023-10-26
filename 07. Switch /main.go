4cc24cbbeeb4dfcf05b1cd6987c22a9aecde5ff3
fdhhhdjd <nguyentientai10@gmail.com>
2023-10-26 16:07:33 +0700

    #7 [ 07 ] Switch

---
+++ 07. Switch /main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+	fmt.Print("Write ", i, " as ")
+
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(31)
+	whatAmI("Tai")
+}
+
+//Todo 1: Write 2 as
+
+//Todo 2: Tow
+
+//Todo 3: It's a weekday
+
+//Todo 4: It's after noon
+
+//Todo 5: I'm a bool , I'm an int, Don't know type string
