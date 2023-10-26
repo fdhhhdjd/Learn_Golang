02e02c3287b31b9e425ffd723d095eed66514610
fdhhhdjd <nguyentientai10@gmail.com>
2023-10-26 14:46:08 +0700

    #2 [ 02 ] Values

---
+++ 02. Values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Nguyen"+"Tien"+"Tai", "go"+"lang")
+
+	fmt.Println("1 + 1", "and", 1+1)
+
+	fmt.Println("7.0/3.0", "and", 7.0/3.0)
+
+	fmt.Println(true && false)
+
+	fmt.Println(true || false)
+}
+
+//Todo 1. NguyenTienTai golang
+//Todo 2. 1 + 1 and 2
+//Todo 3. 7.0/3.0 and 2.3333333333333335
+//Todo 4. false
+//Todo 5. true
