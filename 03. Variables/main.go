19c76c16593cc55b76f99aa6330a931ea037c536
fdhhhdjd <nguyentientai10@gmail.com>
2023-10-26 15:07:05 +0700

    #3 [ 03 ] Variables

---
+++ 03. Variables/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "initial"
+	fmt.Println(a)
+
+	var b, c, d int = 31, 05, 2000
+	fmt.Println(b, c, d)
+
+	var e bool = true
+	fmt.Println(e)
+
+	var f int
+	fmt.Println(f)
+
+	k := "apple"
+	fmt.Println(k)
+}
+
+//Todo 1: initial
+
+//Todo 2: 31 5 2000
+
+//Todo 3: true
+
+//Todo 4: bool = false, string = "", int = 0
+
+//Todo 5: apple
