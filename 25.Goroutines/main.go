43353dc0cb0040ee0e81973b7a51bb47e5e6fe7a
fdhhhdjd <nguyentientai10@gmail.com>
2024-05-24 14:37:54 +0700

    feat: Goroutines

---
+++ 25.Goroutines/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+	f("hello")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(time.Second)
+	fmt.Println("done")
+}
